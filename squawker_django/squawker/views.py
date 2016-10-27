bad4a306e39a9467f1cec006b6a2efd6b89ac7fd
awsmdavid <awsmdavid@gmail.com>
2016-10-27 01:40:13 -0400

    index.html loops through objects; need to fix forms

--- squawker_django/squawker/views.py
+++ squawker_django/squawker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from squawker.models import Squawk
 
@@ -8,8 +8,14 @@ from squawker.models import Squawk
 def index(request):
     context = "yo"
     squawks = Squawk.objects.order_by('id')
-    return render(request, 'squawker/index.html', {"context": squawks})
+    return render(request, 'squawker/index.html', {"squawks": squawks})
 
-def add_squawk(request):
+def add_squawk(request, text):
     context = "yo"
-    return render(request, 'squawker/index.html', {"context": context})
+    # if len(request.form['squawk_text']) > 140:
+    #     abort(400)
+    # use this https://docs.djangoproject.com/en/1.10/topics/forms/
+    s = Squawk(text='yo')
+    s.save()
+    # create db connection and store the squawk
+    return render(request, 'squawker/index.html')
