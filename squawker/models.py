527863928c5b4faa5813b30d1a36815a1fbcf42e
Gabe Air <gabriel.ruttner@gmail.com>
2016-10-30 16:35:24 -0400

    initial commit

---
+++ squawker/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Squawk(models.Model):
+    squawk_text = models.CharField(max_length=140)
+    pub_date = models.DateTimeField('date published', auto_now=True)
+    def __str__(self):
+        return self.squawk_text
