3dbbabb6907f42dbad9758babed3882bb12bc2a0
Carolina Peisch <carolinapeisch@gmail.com>
2016-10-30 22:30:23 -0400

    new files

--- squawks/views.py
+++ squawks/views.py
@@ -1,5 +1,20 @@
-from django.http import HttpResponse
-
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from squawks.models import Squawk
+# from squawker.forms import SquawkForm
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    squawks = Squawk.objects.order_by('-id')
+    return render(request, 'squawker/index.html', {'squawks': squawks})
+
+
+def add_squawk(request):
+    if request.method == 'POST':
+        # TODO: Server side validation
+        # if len(request.POST.get('squawk_text')) > 140:
+        #     abort(400)
+        # Use django ORM api to store squawk
+        s = Squawk(text=request.POST.get('squawk_text'))
+        s.save()
+    return HttpResponseRedirect('/')
