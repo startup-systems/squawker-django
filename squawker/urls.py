17c890a765f0ae0a052fd608104e3e53ccd095b8
Constantin Scholl <constantin@scholl.im>
2016-10-30 20:06:24 -0400

    squawker in root directory

--- squawker/urls.py
+++ squawker/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^squawk/', include('squawk.urls')),
+    url(r'^', include('squawk.urls')),
     url(r'^admin/', admin.site.urls),
 ]
