3fcb6130f77a815ba1e2e3ea581e7f49f2c3e971
Jersyliu <jersyliu@outlook.com>
2016-10-30 23:07:49 -0400

    go pikachu

--- squawker/settings.py
+++ squawker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,10 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'squawker_db',
-		'USER': 'squawker_user',
-		'PASSWORD': 'squawker_pw',
-		'HOST': '',
-		'POST': '',
+        'USER': 'squawker_user',
+        'PASSWORD': 'squawker_pw',
+        'HOST': '',
+        'POST': '',
     }
 }
 
@@ -124,5 +125,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import dj_database_url
 DATABASES['default'] = dj_database_url.config()
