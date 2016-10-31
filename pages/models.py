e57dd93b3dc7524c28dd049e66f9a1b7f47841ff
ganhong <1137361494@qq.com>
2016-10-31 19:10:16 -0400

    migrate to postgresql

--- pages/models.py
+++ pages/models.py
@@ -3,6 +3,5 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Squawker(models.Model):
-	squawker_id = models.IntegerField()
-	squewker_msg = models.CharField(max_length = 140)
+	squawker_msg = models.CharField(max_length = 140)
 	created_date = models.DateTimeField(auto_now = True)
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Squawker
 
-def index(request):
-	return HttpResponse('Hello World')
+def index(request, page_id = 1):
+	latest_squawker_list = Squawker.objects.order_by('-created_date')
+	cnt = len(latest_squawker_list)
+	template = loader.get_template('pages/index.html')
+	_start = (int(page_id) - 1) * 20
+ 	_end = min(len(latest_squawker_list), (int(page_id) - 1) * 20 + 20)
+	context = { 
+		'msgs': latest_squawker_list[_start:_end],
+		'page': page_id,
+		'num': cnt
+	}
+	return HttpResponse(template.render(context, request))
