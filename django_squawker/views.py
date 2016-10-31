21df647f8a2fc219ea1cd37d5f668a7fc1cf2503
Zephyr Liu <youxiang518@gmail.com>
2016-10-31 19:30:38 -0400

    Add pages

--- django_squawker/views.py
+++ django_squawker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.template import loader
 # Create your views here.
 from .models import Posts
@@ -9,18 +9,32 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 def index(request):
     if request.method == 'POST':
         if len(request.POST['comment']) > 140:
-            return redirect('/')
+            return HttpResponseBadRequest('', mimetype='application/json', status=400)
         Posts.objects.create(text=request.POST['comment'])
         return redirect('/')
     latest_posts = Posts.objects.order_by('-date_time')[:]
     template = loader.get_template('django_squawker/index.html')
     context = {
         'greeting_list': latest_posts,
-        'prev_num': 0,
         'next_num': 0,
     }
     return HttpResponse(template.render(context, request))
 
 
 def detail(request, page):
-    return HttpResponse("You're looking at my_args %s." % page)
+    if request.method == 'POST':
+        if len(request.POST['comment']) > 140:
+            return HttpResponseBadRequest('', mimetype='application/json', status=400)
+        Posts.objects.create(text=request.POST['comment'])
+        return redirect('/')
+    latest_posts = Posts.objects.order_by('-date_time')[:]
+    hasNextPage = 0
+    if len(latest_posts) - (int(page) * 20) > 0:
+        hasNextPage = 1
+    template = loader.get_template('django_squawker/index.html')
+    context = {
+        'greeting_list': latest_posts[(int(page) - 1) * 20:int(page) * 20],
+        'next_num': int(page) + 1,
+        'hasNextPage': hasNextPage,
+    }
+    return HttpResponse(template.render(context, request))
