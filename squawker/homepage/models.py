52cedfc0cbd143042438b8e771f6dd0e9dff246b
jersyliu <jersyliu@outlook.com>
2016-10-31 01:47:12 +0000

    haha

--- squawker/homepage/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Mymessage(models.Model):
-	message_id = models.AutoField(primary_key = True)
-	message_text = models.CharField(max_length = 140)
-
-	
--- squawker/urls.py
+++ squawker/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+
 urlpatterns = [
-        url(r'^admin/', admin.site.urls),
-        url(r'^$', include('homepage.urls')),
-	url(r'^zl558-squawker.herokuapp.com', include('homepage.urls')),
-	url(r'^homepage/', include('homepage.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('homepage.urls')),
+    url(r'^zl558-squawker.herokuapp.com', include('homepage.urls')),
+    url(r'^homepage/', include('homepage.urls')),
 ]
