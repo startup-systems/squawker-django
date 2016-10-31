ff7e605e1fa87fbfa9d55733b4b60f352ef8ec4f
ganhong <1137361494@qq.com>
2016-10-31 16:00:09 -0400

    add model

--- pages/models.py
+++ pages/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Squawker(models.Model):
+	squawker_id = models.IntegerField()
+	squewker_msg = models.CharField(max_length = 140)
+	created_date = models.DateTimeField(auto_now = True)
