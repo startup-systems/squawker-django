0fee3f769165c2476f971e951e958965aa6e43cb
rwg58 <rwg58@mail.missouri.edu>
2016-10-30 22:15:38 -0400

    fix problem

--- polls/models.py
+++ polls/models.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 # Create your models here.
 class Squawker(models.Model):
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=140)
     postdate = models.DateTimeField("postdate", default=datetime.now, blank=True)
