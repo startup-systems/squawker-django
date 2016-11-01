afbcf4195ecec573a398ac3956cebe78fc4b657d
ylynn <313079231@qq.com>
2016-11-01 07:43:11 +0000

    o

--- sapp/views.py
+++ sapp/views.py
@@ -6,13 +6,13 @@ import time
 
 def index(request):
     if request.method == 'POST':
-        post = request.POST['getPosts']
-        if len(post) > 140:
+        post_msg = request.POST['get_posts']
+        if len(post_msg) > 140:
             response = HttpResponse('Your post should be less than 140 characters.')
             response.status_code = 400
             return response
         t = time.time()
-        posts(message=post, post_time=t).save()
-    newPosts = posts.objects.order_by('-post_time')[:20]
-    viewPosts = {'newPosts': newPosts}
-    return render(request, 'sapp/index.html', viewPosts)
+        posts(message=post_msg, post_time=t).save()
+    new_posts = posts.objects.order_by('-post_time')[:20]
+    context = {'new_posts': new_posts}
+    return render(request, 'sapp/index.html', context)
