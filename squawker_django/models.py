898069597902c3160a924e637e0229cdaccb04b5
ganhong <1137361494@qq.com>
2016-10-31 23:10:49 -0400

    code complete

--- squawker_django/models.py
+++ squawker_django/models.py
@@ -1,3 +1,10 @@
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+class Squawker(models.Model):
+	squawker_msg = models.CharField(max_length = 140)
+	created_date = models.DateTimeField(auto_now = True)
+
+	def __str__(self):
+		return squawker_msg
