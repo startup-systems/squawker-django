bf077dc76eff804b7096c355d6bc8b897c3a626e
vagrant <jz762@cornell.edu>
2016-11-01 23:06:43 +0000

    please codeclimate

--- squawker/squawker/settings.py
+++ squawker/squawker/settings.py
@@ -75,12 +75,11 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3'),}
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3'), }
 # {
 # 'ENGINE': 'django.db.backends.sqlite3',
 # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 # }
-
 
 
 # Password validation
