c6341e60a888ad954a8d63e7d8a67824ebc8ba77
vagrant <jz762@cornell.edu>
2016-11-01 23:04:23 +0000

    please codeclimate

--- squawker/posts/admin.py
+++ squawker/posts/admin.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 class PostModelAdmin(admin.ModelAdmin):
     list_display = ("title", "updated", "timestamp")
- 
+
     class Meta:
         model = Post
 admin.site.register(Post)
--- squawker/posts/models.py
+++ squawker/posts/models.py
@@ -8,6 +8,6 @@ class Post(models.Model):
     content = models.TextField(max_length=140)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    
+
     def __str__(self):
         return self.title
