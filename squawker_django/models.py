954b44ccf21be71f7f1a0043436317156f83d511
vagrant <1137361494@qq.com>
2016-11-01 04:12:24 +0000

    exaustedgit add .!

--- squawker_django/models.py
+++ squawker_django/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Squawker(models.Model):
-    squawker_msg = models.CharField(max_length = 140)
-    created_date = models.DateTimeField(auto_now = True)
+    squawker_msg = models.CharField(max_length=140)
+    created_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return squawker_msg
