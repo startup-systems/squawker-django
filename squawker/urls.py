86ecf910bdc53d0168a74f399d0c3c7b77da8da0
Evan Kesten <evan@challengage.com>
2016-10-31 20:47:41 -0400

    restructure

--- squawker/urls.py
+++ squawker/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+
+from . import views
 
 urlpatterns = [
-    url(r'^squawker/', include('squawker.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
