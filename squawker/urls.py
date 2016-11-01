be1ce574f776491e3bda9ec903f5f71f9c223631
ylynn <313079231@qq.com>
2016-11-01 03:25:28 +0000

    squawker

--- squawker/urls.py
+++ squawker/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from sapp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('sapp.urls')),
+    url(r'^', views.index),
 ]
