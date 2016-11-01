3589b2f5187ce97b42e483b119169e251f91891c
Evan Kesten <evan@challengage.com>
2016-10-31 21:05:15 -0400

    more style fun

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 DATABASES = {
-    "default":dj_database_url.config(default='sqlite:///db.sqlite3'), 
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 # Application definition
