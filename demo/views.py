7182f2aa7d2b059b6336c552d7050797cefcccbb
dydcfg <dydcfg@gmail.com>
2016-11-01 05:20:31 +0000

    Extra

--- demo/views.py
+++ demo/views.py
@@ -8,7 +8,7 @@ from django.views import defaults
 from .models import Squawk
 
 
-def index(request, page_id=0):
+def index(request, page_id="1"):
     if len(request.POST) > 0:
         s = request.POST['squawk']
         print(s)
@@ -19,9 +19,24 @@ def index(request, page_id=0):
         t = Squawk(squawk_text=request.POST['squawk'], pub_date=timezone.now())
         t.save()
 
+    pageN = int(page_id)
     squawk_list = Squawk.objects.order_by('-pub_date')
+    numSquawks = len(squawk_list)
+    squawk_list = squawk_list[(pageN - 1) * 20: min(numSquawks, pageN * 20)]
+    haveNext = 0
+    havePre = 0
+
+    if pageN > 1:
+        havePre = 1
+
+    if pageN * 20 < numSquawks:
+        haveNext = 1
+
     context = {
         'squawk_list': squawk_list,
-        'page_id': page_id,
+        'pageN': pageN,
+        'numSquawks': numSquawks,
+        'haveNext': haveNext,
+        'havePre': havePre,
     }
     return render(request, 'demo/index.html', context)
