779f700fe957a5ec7370be6de7f4fb39680999e4
Weiming <zwm1216@gmail.com>
2016-11-01 02:26:05 -0400

    k

--- squawker/settings.py
+++ squawker/settings.py
@@ -130,6 +130,4 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'),)
