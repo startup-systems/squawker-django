1e6272fbe1f0e3ca7da13486b66e3bcb13e12227
ylynn <313079231@qq.com>
2016-11-01 06:43:08 +0000

    heroku

--- sapp/views.py
+++ sapp/views.py
@@ -6,7 +6,7 @@ import time
 
 def index(request):
     if request.method == 'POST':
-        post = request.POST['posts']
+        post = request.POST['getPosts']
         if len(post) > 140:
             response = HttpResponse('Your post should be less than 140 characters.')
             response.status_code = 400
@@ -14,5 +14,5 @@ def index(request):
         t = time.time()
         posts(message=post, post_time=t).save()
     newPosts = posts.objects.order_by('-post_time')[:20]
-    viewposts = {'newPosts': newPosts}
-    return render(request, 'sapp/index.html', viewposts)
+    viewPosts = {'newPosts': newPosts}
+    return render(request, 'sapp/index.html', viewPosts)
