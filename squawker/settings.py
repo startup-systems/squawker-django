d5151a0ff68e1e4d306bdda9c919bfbd14dde98e
Federico Zarfati <alogiconi@gmail.com>
2016-10-31 22:29:48 -0400

    change db

--- squawker/settings.py
+++ squawker/settings.py
@@ -83,7 +83,10 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 
 # replace the DATABASES config
 DATABASES = {
-    "default": dj_database_url.config(),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
