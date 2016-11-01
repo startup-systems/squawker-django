bcf9d06e426a07541255ad31e394ee0b235ed8cb
vagrant <1137361494@qq.com>
2016-11-01 04:07:53 +0000

    style2

--- squawker_django/urls.py
+++ squawker_django/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^post/$', views.save_squawker, name='postSq'),
-	url(r'^(?P<page_id>[0-9]+)/$', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^post/$', views.save_squawker, name='postSq'),
+    url(r'^(?P<page_id>[0-9]+)/$', views.index, name='index'),
 ]
+
