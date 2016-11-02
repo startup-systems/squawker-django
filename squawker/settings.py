97a4870184f3a7bdf3df870ba46a95a09daef150
ragodrich <rag299@cornell.edu>
2016-11-02 18:08:52 +0000

    first try

--- squawker/settings.py
+++ squawker/settings.py
@@ -13,11 +13,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 
-# replace the DATABASES config
-DATABASES = {
-    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
-}
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,10 +76,7 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
 
 
