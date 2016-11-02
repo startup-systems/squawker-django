a8f0b6622b97d808c7610adc2b22b3711315d04d
AltmerX <sabrina5426@live.com>
2016-11-02 14:47:42 -0400

    Fixed static problem

--- squawker/settings.py
+++ squawker/settings.py
@@ -117,4 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/' 
+# Extra places for collectstatic to find static files. 
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'), 
+) 
+# Simplified static file serving. 
+# https://warehouse.python.org/project/whitenoise/ 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
