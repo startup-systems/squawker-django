426d6ef4bccf5c0f36e5493c274695b5169c519c
virtuositeit <yougeoric@gmail.com>
2016-11-02 23:05:05 +0000

    deleted comment

--- tweet/models.py
+++ tweet/models.py
@@ -3,7 +3,6 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 class Posts(models.Model):
-#   'jc3256-squawker.herokuapp.com'
     post_text = models.CharField(max_length=140)
     post_time = models.DateTimeField('time published')
 
