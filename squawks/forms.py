e40df5e78d80d21c0795e2a2913fd4e45eaf69e5
trvslhlt <trvslhlt@gmail.com>
2016-11-02 11:23:26 -0400

    Code Climate cleanup

--- squawks/forms.py
+++ squawks/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Squawk
 
+
 class SquawkForm(forms.ModelForm):
 
     class Meta:
--- squawks/models.py
+++ squawks/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 
 class Squawk(models.Model):
     text = models.TextField(max_length=140)
