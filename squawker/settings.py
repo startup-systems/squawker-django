2ddc7591d75b359b5664148f90f52b7c05709125
42lince <42.duxiaoxi@gmail.com>
2016-11-02 01:40:01 -0400

    change allowed hosts

--- squawker/settings.py
+++ squawker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'rd*^7#k%v&q&qwpa00^1jj-5=7cfe&+*5wlggar-b5d+y$-p!y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'https://xd47-squawker.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = [u'xd47-squawker.herokuapp.com', 'localhost']
 
 
 # Application definition
