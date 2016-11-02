2bc1999bd6d830c18ce5251eb9300182af9d24d2
BKrieger73 <bk494@cornell.edu>
2016-11-02 19:21:46 +0000

    added postgres support

--- squawker/settings.py
+++ squawker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,13 +76,23 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
+}'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd428behqlli224',
+        'USER': 'lnlbfofxclkqtr',
+        'PASSWORD': 'PrBGoDZmllr4iMVDfIhAUgLHXQ',
+        'HOST': 'ec2-54-163-226-121.compute-1.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
