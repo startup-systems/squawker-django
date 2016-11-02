a137e79983ad2402028b06c6e79735a1d0406448
trvslhlt <trvslhlt@gmail.com>
2016-11-01 23:55:26 -0400

    mvp

--- squawks/views.py
+++ squawks/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseBadRequest
 from squawks.models import Squawk
+from .forms import SquawkForm
 
 
 def squawk_list(request):
-    squawks = Squawk.objects.all().order_by('created_date')
-    return render(request, 'squawks/squawks_list.html', {'squawks': squawks})
+
+    def get_squawk_list(request):
+        squawks = Squawk.objects.all().order_by('-created_date')
+        return render(request, 'squawks/squawks_list.html', {'squawks': squawks})
+
+    if request.method == 'POST':
+        form = SquawkForm(request.POST)
+        if form.is_valid():
+            squawk = form.save()
+        else:
+            return HttpResponseBadRequest()
+        return get_squawk_list(request)
+    else:
+        return get_squawk_list(request)
