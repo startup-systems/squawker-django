7382a80e918b7d4ad25c016d7a916a5cf2368fa9
trvslhlt <trvslhlt@gmail.com>
2016-11-02 12:44:24 -0400

    whitenoise in wsgi

--- squawker/wsgi.py
+++ squawker/wsgi.py
@@ -11,8 +11,8 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squawker.settings")
 
-application = StaticFilesHandler(get_wsgi_application())
+application = DjangoWhiteNoise(get_wsgi_application())
