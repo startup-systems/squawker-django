0f9739ff7f4b9c477f1a40f35279c3353a91cc0c
trishalaneeraj <trishalaneeraj@gmail.com>
2016-11-02 21:12:26 +0000

    Squawk

--- squawkermsg/urls.py
+++ squawkermsg/urls.py
@@ -1,7 +1,19 @@
+# from django.conf.urls import url
+
+# from . import views
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+# ]
+
+
+
+
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^page/(?P<num>[0-9]+)/$', views.index, name='indexpage'),
 ]
