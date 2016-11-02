25f3c99b8672fe9d8cbbe2107e5b4cf8395af047
ragodrich <rag299@cornell.edu>
2016-11-02 20:45:37 +0000

    try

--- squawker/settings.py
+++ squawker/settings.py
@@ -120,3 +120,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
