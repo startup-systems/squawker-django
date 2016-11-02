80ef9153b3cb83fbf863525d911cf5b55bb0aa33
Diego Wolfsdorf <diegow88@github.com>
2016-11-02 03:25:36 +0000

    Corrijo bug

--- squawk/views.py
+++ squawk/views.py
@@ -5,20 +5,24 @@ from django.http import Http404
 from .models import Squawk
 from .forms import SquawkForm
 import time
+from math import ceil
 
 ENTRIES_PER_PAGE = 2
 
 def index(request, num="1"):
     num=int(num)
+    status=200
     if request.method == 'POST':
         form = SquawkForm(request.POST)
         if form.is_valid():
             s = Squawk(message=form.cleaned_data['message'], create_at=int(time.time()))
             s.save()
+        else:
+            status=400
     else:
         form = SquawkForm()
 
-    last_page = Squawk.objects.count() / ENTRIES_PER_PAGE
+    last_page = ceil(Squawk.objects.count() / ENTRIES_PER_PAGE)
     offset = ENTRIES_PER_PAGE * (num-1)
     limit = offset + ENTRIES_PER_PAGE
 
@@ -35,4 +39,4 @@ def index(request, num="1"):
         'last_page': last_page,
         'form':form
     }
-    return render(request, 'squawk/index.html', context)
+    return render(request, 'squawk/index.html', context, status=status)
