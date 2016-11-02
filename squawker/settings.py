1e192cb45bef5452dd599bff4548a4a39b3ca848
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-11-02 17:45:35 -0400

    changing debug to true

--- squawker/settings.py
+++ squawker/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9tkqg+llk+su&%i-m^j91%82bq5)u-yyfhiboz40-e+18g=@t!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
