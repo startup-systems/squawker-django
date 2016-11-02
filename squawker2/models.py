7b5e766f150f6ed2d27c61ebd65f12c4d41e2463
QueueOverflow <Titan@dhcp-ccc-2930.eduroam.cornell.edu>
2016-11-02 17:24:17 -0400

    hi

--- squawker2/models.py
+++ squawker2/models.py
@@ -8,6 +8,5 @@ from django.db import models
 class Squawks(models.Model):
     squawk = models.CharField(max_length=140)
     timestamp = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
+        def __str__(self):
         return self.squawk
