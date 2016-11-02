2ad93281b0993706bde36309c6b0d08575ea980a
kristenwang <kristenwang23@gmail.com>
2016-11-02 14:20:45 -0400

    make it better

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Posts
+from .models import posts
+import time
 # Create your views here.
 def index(request):
     if request.method == 'POST':
@@ -10,7 +11,7 @@ def index(request):
             response.status_code = 400
             return response
         t = time.time()
-        Posts(message=post_msg, post_time=t).save()
-    new_posts = Posts.objects.order_by('-post_time')[:20]
+        posts(message=post_msg, post_time=t).save()
+    new_posts = posts.objects.order_by('-post_time')[:20]
     context = {'new_posts': new_posts}
     return render(request, 'post/index.html', context)
