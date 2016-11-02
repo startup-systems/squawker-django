70e10baa0540338d472b8c5b616958ad6a18a133
AltmerX <sabrina5426@live.com>
2016-11-02 14:54:35 -0400

    Fixed url problem

--- squawker/urls.py
+++ squawker/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-     url(r'^squawkerapp/', include('squawkerapp.urls')),
+     url(r'^$', include('squawkerapp.urls')),
      url(r'^admin/', admin.site.urls),
 ]
