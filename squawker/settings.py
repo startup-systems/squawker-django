6310c389842fa37d488e027c57c23aa6eaf780e6
yz1339 <yz1339@nyu.edu>
2016-11-02 03:46:26 +0000

    trying

--- squawker/settings.py
+++ squawker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 import dj_database_url
+db_from_env = dj_database_url.config()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
