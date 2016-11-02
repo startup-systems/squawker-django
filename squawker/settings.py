d89641191df03a5e0277358ff23a494db6c7bf59
Elya Pardes <elyapardes@gmail.com>
2016-11-02 22:37:22 +0000

    commit

--- squawker/settings.py
+++ squawker/settings.py
@@ -102,10 +102,10 @@ DATABASES = {
 
 # Old database, before switching to Postgresql:
 # DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+# }
 # }
 
 
