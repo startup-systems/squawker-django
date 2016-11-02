caba8867ff28ac13b8618e090b22006d7c9c5c0e
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-11-02 18:02:40 -0400

    databse

--- squawker/settings.py
+++ squawker/settings.py
@@ -85,6 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
