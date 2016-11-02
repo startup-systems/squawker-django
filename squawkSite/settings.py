f115fb2dce14f957541a5de961ba7f3b04491253
Gabe Air <gabriel.ruttner@gmail.com>
2016-11-01 21:35:05 -0400

    heroku setup

--- squawkSite/settings.py
+++ squawkSite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+# add this near the top
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,12 +78,8 @@ WSGI_APPLICATION = 'squawkSite.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
