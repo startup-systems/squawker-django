6bd64c307a83f67c8e53ba8627f5dd18445a9e7f
Kiyan <krajabi09@gmail.com>
2016-11-02 19:53:56 +0000

    first

---
+++ main/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+class Squawk(models.Model):
+	post = models.CharField(max_length=140)
+	timestamp= models.FloatField()
---
+++ main/views.py
@@ -0,0 +1,20 @@
+import time
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Posts
+
+
+
+def index(request):
+    if request.method == 'POST':
+        post = request.POST['text_box_input']
+        if len(post) > 140:
+            response = HttpResponse('cannot exceed 140 characters)')
+            response.status_code = 400
+            return response
+        t = time.time()
+        Posts(message=post, timestamp=t).save()
+    maxP = 20
+    feed = Posts.objects.order_by('-timestamp')[:maxP]
+    context = {'feed': feed}
+    return render(request, 'main/index.html', context)
