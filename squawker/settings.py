b5c1b1590bb7476113be0ee9a289227deec8f834
Arindrima Datta <arindrima@MacBook-Pro.local>
2016-11-02 18:21:51 -0400

    some change

--- squawker/settings.py
+++ squawker/settings.py
@@ -83,8 +83,8 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
