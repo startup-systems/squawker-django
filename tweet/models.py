993b05de359c7860346fe587e76688e517ca57eb
virtuositeit <yougeoric@gmail.com>
2016-11-02 22:45:39 +0000

    debug

--- tweet/models.py
+++ tweet/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
-class tweet_osts(models.Model):
+class Posts(models.Model):
+#   'jc3256-squawker.herokuapp.com'
     post_text = models.CharField(max_length=140)
     post_time = models.DateTimeField('time published')
 
