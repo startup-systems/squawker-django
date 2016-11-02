40ec4233c26be4a7ebd0a8e05f9da4297692bc7b
Lannie Miao <ym232@cornell.edu>
2016-11-02 02:23:38 +0000

    adding whitespace

--- squawkerapp/views.py
+++ squawkerapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect,HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from .models import postSquawk
 
 
