84feb54c4846cc2473af63b85149d3584d6d66fc
Diego Wolfsdorf <diegow88@github.com>
2016-11-02 03:59:40 +0000

    Changed DB setting

--- squawker/settings.py
+++ squawker/settings.py
@@ -74,16 +74,20 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
+# add this near the top
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+
+# replace the DATABASES config
+DATABASES = {
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
