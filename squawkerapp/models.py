6cb4d213be4afc41a87f1c16ff1420e13c43c190
AltmerX <sabrina5426@live.com>
2016-11-02 19:14:58 -0400

    migrate

--- squawkerapp/models.py
+++ squawkerapp/models.py
@@ -3,6 +3,6 @@ from datetime import datetime
 
 class Squawk(models.Model):
     squawk_text = models.CharField(max_length = 140)
-    timestamp = models.DateTimeField('date published')
+    timestamp = models.DateTimeField(default = datetime.now, blank = True)
     #def __str__(self):
     #   return self.squawk_text + "      " + self.timestamp
