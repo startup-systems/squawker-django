06a2a5bbf07d844e6769b356e7546e49b829c7da
arp122 <arpit.sabherwal@gmail.com>
2016-11-02 06:42:07 +0000

    my first commit

--- postfeed/models.py
+++ postfeed/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     auto_id = models.AutoField(primary_key=True)
     msg = models.CharField(max_length=140)
--- squawker/settings.py
+++ squawker/settings.py
@@ -128,4 +128,3 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
