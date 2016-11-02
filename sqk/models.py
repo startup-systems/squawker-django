ac0f02a619584fa93803667cc922e33f5d473965
Yan-J <jiangcw@jydeMacBook-Air.fios-router.home>
2016-11-02 11:04:45 +0800

    update

--- sqk/models.py
+++ sqk/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class posts(models.Model):
-	message = models.CharField(max_length=140)
-	post_time = models.FloatField()
+    message = models.CharField(max_length=140)
+    post_time = models.FloatField()
