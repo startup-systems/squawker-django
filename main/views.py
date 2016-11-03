f44d236e6fd51e50aefa60846e32a7f2bd26134b
Mario Rial <mr2224@cornell.edu>
2016-11-02 20:07:42 -0400

    Final

--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 # encoding: utf-8
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.core.paginator import Paginator
 from main.models import Squawk
 
 # Create your views here.
+COUNT = 10
 
 def index(request):
+    page_number = 1
+    if "page" in request.GET:
+        page_number = int(request.GET["page"])
+    
     if request.method == "POST":
         text = request.POST["text"]
         if len(text)>140:
@@ -15,5 +21,8 @@ def index(request):
         return redirect(index)
     else:
         squawks = Squawk.objects.order_by('-time')
-        return render(request, "show_entries.html", {"entries":squawks})    
+        p = Paginator(squawks, COUNT)
+        page = p.page(page_number)
+        objects = page.object_list
+        return render(request, "show_entries.html", {"entries":objects, "has_previous":page.has_previous(), "has_next":page.has_next(), "page":page_number})
     
