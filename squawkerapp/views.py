cb5a4286c115682bd5f3d61c1a23bf55283d30d4
AltmerX <sabrina5426@live.com>
2016-11-02 20:12:54 -0400

    Fixed squawk exceeding 140 chars

--- squawkerapp/views.py
+++ squawkerapp/views.py
@@ -7,8 +7,9 @@ from .models import Squawk
 def index(request):
     if request.method == 'POST':
         newSquawk = request.POST.get('new_squawk')
-        newPost = Squawk(squawk_text=newSquawk)
-        newPost.save()
+        if len(newSquawk) <= 140:
+            newPost = Squawk(squawk_text=newSquawk)
+            newPost.save()
     latest_squawk_list = Squawk.objects.order_by('-timestamp')[:20]
     context = {'latest_squawk_list': latest_squawk_list}
     return render(request, 'squawkerapp/index.html', context)
