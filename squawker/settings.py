adceb264a1d7b704b5f90c9327b03bd435cc5820
vagrant <np327@cornell.edu>
2016-11-04 20:02:21 +0000

    heroku

--- squawker/settings.py
+++ squawker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'aain_)izdg9=nl@4cqwre1c@ez69oo(s+ja!6z8k@*_qyx6w$z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['np327-squawker.herokuapp.com']
 
 
 # Application definition
@@ -81,8 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-heroku config:set DISABLE_COLLECTSTATIC=1
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
