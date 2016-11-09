2687dea7751c42e024cbf22b5fe61007798153c7
FVCproductions <hello@fvcproductions.com>
2016-11-08 21:48:14 -0500

    last try
    Signed-off-by: FVCproductions <hello@fvcproductions.com>

--- tweet/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class squawkForm(forms.Form):
-    msg = forms.TextField(label='enter your squawk: ', max_length=140)
--- tweet/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class postSquawks(models.Model):
-    msg = models.TextField(max_length=140)
