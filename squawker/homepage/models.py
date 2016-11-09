69f8dbf2de2e84e7e367a97c59852b581833e9b2
noopurP <parikh.noopur18@gmail.com>
2016-11-09 09:20:54 -0500

    Update models.py

--- squawker/homepage/models.py
+++ squawker/homepage/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Squawk(models.Model):
-	txt = models.CharField(max_length=140)
-	date = models.DateTimeField(auto_now_add=True)
-	
-	def __str__(self):
-		return self.txt
+    txt = models.CharField(max_length=140)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.txt
