019ca57eb34c5f95087bedc734cd319411d97283
ragodrich <rag299@cornell.edu>
2016-11-09 00:21:47 +0000

    finally

--- PostSquawk/models.py
+++ PostSquawk/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class postSquawk(models.Model):
     msg = models.CharField(max_length=140)
+
+    def __str__(self):
+         return self.msg
 # Create your models here.
