2d6ef50d24633443054785191efeabfb3bf05b27
noopurP <parikh.noopur18@gmail.com>
2016-11-08 20:20:31 -0500

    Update views.py

--- squawker/homepage/views.py
+++ squawker/homepage/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from homepage.models import Squawk
 
+
 def index(request):
-	return render(request, "homepage/home.html")
+    return render(request, "homepage/home.html")
