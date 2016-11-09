ff911146a4de5ab3b0356b7cc9ff6ecebe4a6f73
noopurP <parikh.noopur18@gmail.com>
2016-11-08 20:18:53 -0500

    Update settings.py

--- squawker/squawker/settings.py
+++ squawker/squawker/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'homepage',
+    'homepage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
