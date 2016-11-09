b1ba601381b5fddab75fa9d9ebd72f5ac6aceef0
noopurP <parikh.noopur18@gmail.com>
2016-11-08 20:23:33 -0500

    Update urls.py

--- squawker/homepage/urls.py
+++ squawker/homepage/urls.py
@@ -3,4 +3,4 @@ from . import views
 from django.views.generic import ListView, DetailView
 from homepage.models import Squawk
 
-urlpatterns = [ url(r'^', ListView.as_view(queryset=Squawk.objects.all().order_by("-date"), template_name="homepage/home.html"))]
+urlpatterns = [url(r'^', ListView.as_view(queryset=Squawk.objects.all().order_by("-date"), template_name="homepage/home.html"))]
