6f56f79291b8a6115c91d3aaaf5e6f2f3db3d2de
noopurP <parikh.noopur18@gmail.com>
2016-11-09 08:29:08 -0500

    Update views.py

--- squawker/homepage/views.py
+++ squawker/homepage/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
-from homepage.models import Squawk
+from django.views.generic import RedirectView
+from .models import Squawk
 
 
 def index(request):
-    return render(request, "homepage/home.html")
+    object_list = {'All_squawks': Squawk.objects.order_by('-date')}
+    return render(request, 'homepage/home.html', object_list)
+
+
+def squawker(request):
+    q = Squawk(txt=request.POST['content'])
+    if len(q.txt) > 140:
+        response = HttpResponse('Enter only 140 characters')
+        response.status_code = 400
+        return response
+    q.save()
+    return index(request)
