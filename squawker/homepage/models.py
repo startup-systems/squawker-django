bd02b297366db1ccf71c63d6ef3d24e38d91a6b8
noopurP <parikh.noopur18@gmail.com>
2016-11-08 23:18:47 -0500

    Still_Trying

--- squawker/homepage/models.py
+++ squawker/homepage/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Squawk(models.Model):
 	txt = models.CharField(max_length=140)
-	date = models.DateTimeField()
+	date = models.DateTimeField(auto_now_add=True)
 	
 	def __str__(self):
 		return self.txt
