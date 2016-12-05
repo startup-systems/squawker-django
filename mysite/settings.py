80e3d59bb062c6e67e5a0d8b03699b11c71d72ee
Gautam Chheda <gc463@cornell.edu>
2016-12-05 02:27:14 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'squawker.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'squawker.wsgi.application'
 
 
 # Database
