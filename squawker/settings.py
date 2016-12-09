3f0949833d517f34bb2ef1fdd129f8ae88078d90
avash <avash.rao@gmail.com>
2016-12-09 03:56:38 -0500

    db change for heroku

--- squawker/settings.py
+++ squawker/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'squawker.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(default='sqlite:///db.sqlite3'),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
